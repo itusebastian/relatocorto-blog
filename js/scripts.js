898ef4927813365c30d8b76cce8056bdecc4d75f
Sebastian Iturralde <itusebastian@gmail.com>
2025-01-08 15:31:27 -0500

    Added About & News Pages

--- js/scripts.js
+++ js/scripts.js
@@ -383,7 +383,7 @@ jQuery(document).ready(function ($) {
   }
 
   // Load dynamic content and initialize scripts in sequence
-  $.when(loadHeader(), loadOverlays(), loadWidgets())
+  $.when(loadHeader(), loadSidebar(), loadOverlays(), loadWidgets())
     .done(function () {
       // Initialize the menu after all components are loaded
       initializeMenu();
